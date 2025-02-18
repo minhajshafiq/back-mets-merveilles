662473a87c9b2c1d5e3f81e2301bc4e9347120ea
minhajshafiq <minhaj93500@gmail.com>
2025-02-18 17:18:00 +0100

    fix: front connection

--- src/main/java/org/metsetmerveilles/presentation/controller/MenuController.java
+++ src/main/java/org/metsetmerveilles/presentation/controller/MenuController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "https://front-mets-merveilles.vercel.app/")
+@CrossOrigin(origins = "https://front-mets-merveilles.vercel.app")
 @RequestMapping("/menu")
 public class MenuController {
 
